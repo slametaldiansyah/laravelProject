2a631a1d8dc876ae8726fc4173237cbab3b252ab
Slamet Aldiansyah <slametaldiansyah96@gmail.com>
2021-01-08 17:37:43 +0700

    add template

--- ubuntu-laravel/routes/web.php
+++ ubuntu-laravel/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
+});
+
+Route::get('/home', function () {
+    return view('layouts.master');
 });
